30a5555790c0b1c66919f39b5f8fa8bb376e778f
DA SILVA <k.dasilva@cgi.com>
2023-12-21 13:43:06 +0100

    Loader

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const animatedTitle = document.getElementById('animatedTitle');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const animatedTitle = document.getElementById('animatedTitle');
   
-    animatedTitle.addEventListener('mouseenter', () => {
-      animatedTitle.style.animationPlayState = 'paused';
-    });
+//     animatedTitle.addEventListener('mouseenter', () => {
+//       animatedTitle.style.animationPlayState = 'paused';
+//     });
   
-    animatedTitle.addEventListener('mouseleave', () => {
-      animatedTitle.style.animationPlayState = 'running';
-    });
+//     animatedTitle.addEventListener('mouseleave', () => {
+//       animatedTitle.style.animationPlayState = 'running';
+//     });
   
-    // Effet de curseur
-    document.addEventListener('mousemove', (event) => {
-      const ghostElement = document.querySelector('.ghost');
+//     // Effet de curseur
+//     document.addEventListener('mousemove', (event) => {
+//       const ghostElement = document.querySelector('.ghost');
   
-      if (ghostElement) {
-        ghostElement.style.left = `${event.clientX}px`;
-        ghostElement.style.top = `${event.clientY}px`;
-      }
-    });
+//       if (ghostElement) {
+//         ghostElement.style.left = `${event.clientX}px`;
+//         ghostElement.style.top = `${event.clientY}px`;
+//       }
+//     });
+//   });
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const loader = document.getElementById('loader');
+    const mainContent = document.getElementById('main-content');
+  
+    // Masquer la page de chargement initialement
+    loader.style.display = 'none';
+  
+    // Afficher la page de chargement pendant le chargement initial
+    loader.style.display = 'block';
+  
+    // Simuler le chargement (ajoutez le code réel pour charger vos ressources ici)
+    setTimeout(function () {
+      // Masquer la page de chargement une fois le chargement terminé
+      loader.style.display = 'none';
+      
+      // Afficher le contenu principal
+      mainContent.style.display = 'block';
+    }, 3000); // Simulez un chargement de 3 secondes (à remplacer par le chargement réel)
   });
   
