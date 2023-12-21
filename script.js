8adf73a5bdc6714c1749dc41074d4332bdcdd0ab
DA SILVA <k.dasilva@cgi.com>
2023-12-21 11:56:43 +0100

    update anim

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   const gallery = document.querySelector('.gallery');
-
-//   fetch("https://api.unsplash.com/photos/?client_id=NYUR-fDgEwXSYbh7mGxiwlvc_soA_2bxFJ8dLjzMc4M")
-//       .then(response => response.json())
-//       .then(data => {
-//           data.forEach(photo => {
-//               const img = document.createElement('img');
-//               img.src = photo.urls.small;  
-//               img.alt = photo.description;
-//               gallery.appendChild(img);
-//           });
-//       })
-//       .catch(error => console.error('Error:', error));
-// });
-// JavaScript pour l'interaction avec le titre
-
-const animatedTitle = document.getElementById('animatedTitle');
-
-animatedTitle.addEventListener('mouseenter', () => {
-  animatedTitle.style.animationPlayState = 'paused';
-});
-
-animatedTitle.addEventListener('mouseleave', () => {
-  animatedTitle.style.animationPlayState = 'running';
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const animatedTitle = document.getElementById('animatedTitle');
+  
+    animatedTitle.addEventListener('mouseenter', () => {
+      animatedTitle.style.animationPlayState = 'paused';
+    });
+  
+    animatedTitle.addEventListener('mouseleave', () => {
+      animatedTitle.style.animationPlayState = 'running';
+    });
+  
+    // Effet de curseur
+    document.addEventListener('mousemove', (event) => {
+      const ghostElement = document.querySelector('.ghost');
+  
+      if (ghostElement) {
+        ghostElement.style.left = `${event.clientX}px`;
+        ghostElement.style.top = `${event.clientY}px`;
+      }
+    });
+  });
+  
